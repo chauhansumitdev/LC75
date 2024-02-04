cb0eacc94415f9adefb58658918bc3233c16dd61
Sumit Chauhan <chauhansumit9009@gmail.com>
2024-02-04 23:18:23 +0530

    .

---
+++ Strings/ReverseWordsInAString.java
@@ -0,0 +1,17 @@
+public class ReverseWordsInAString {
+    public String reverseWords(String s) {
+        String sent = s.trim();
+        String arr[] = sent.split("\\s+");
+        StringBuilder str = new StringBuilder();
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            str.append(arr[i]);
+            
+            if (i > 0) {
+                str.append(" ");
+            }
+        }
+        
+        return str.toString();
+    }
+}
